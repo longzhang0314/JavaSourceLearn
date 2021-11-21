bfca40ce25858f15c81df1865754ef5ac0b504be
longzhang <dravenzl@163.com>
2021-11-21 22:16:58 +0800

    项目初始化

---
+++ src/cn/zl/test/hashmap/Test.java
@@ -0,0 +1,16 @@
+package cn.zl.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, Double> hashmap = new HashMap<>();
+
+        hashmap.put("K1", 0.1);
+        hashmap.put("K2", 0.2);
+        hashmap.put("K3", 0.3);
+        hashmap.put("K4", 0.4);
+    }
+}
